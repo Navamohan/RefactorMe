c8d31cc4de4c9025df64fc749343aa5d2a5a9bac
Navamohan <navamohank@gmail.com>
2016-06-14 22:10:08 +1200

    Add files via upload

---
+++ refactor-me/Initialisation/ServicesModule.cs
@@ -0,0 +1,21 @@
+﻿using Ninject.Modules;
+using Ninject.Web.Common;
+using ProductManagement.Models;
+
+namespace ProductManagement.Initialisation
+{
+    /// <summary>
+    /// Services Module class to perform initializations
+    /// </summary>
+    public class ServicesModule: NinjectModule
+    {
+        /// <summary>
+        /// Overloaded Ninject Module used for DI mapping Types 
+        /// </summary>
+        public override void Load()
+        {
+            Bind<IProductsRepository>().To<ProductsRepository>().InRequestScope();
+            Bind<IProductOptionsRepository>().To<ProductOptionsRepository>().InRequestScope();
+        }
+    }
+}
